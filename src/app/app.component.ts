839b238c76dd55005802b1e0cc68d00dcf6f5098
Kevin <changgkgx@gmail.com>
2018-09-08 13:16:57 -0700

    test git modification purpose only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
+//test git
 export class AppComponent {
   title = '124553';
   greeting = 'This is the first example of Angular'
